a213f8e1cbed620c5ff5872716c37cd634a2dac0
Christoph Welcz <christoph@welcz.de>
2019-08-07 02:36:16 +0200

    using record syntax

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import           Data.List
 import           Data.Validation
+import           Prelude         hiding (error)
 
 type FromAddress = Address
 
@@ -20,16 +21,16 @@ data Error
   | MustContain String
   deriving (Show)
 
-data FieldError =
-  FieldError Error
-             String
-  deriving (Show)
+data FieldError = FieldError
+  { error     :: Error
+  , fieldName :: String
+  } deriving (Show)
 
-data Email =
-  Email FromAddress
-        ToAddress
-        Body
-  deriving (Show)
+data Email = Email
+  { from :: FromAddress
+  , to   :: ToAddress
+  , body :: Body
+  } deriving (Show)
 
 main :: IO ()
 main = do
@@ -42,12 +43,12 @@ main = do
 validateAddress :: String -> String -> Validation [FieldError] Address
 validateAddress address which
   | "@" `isInfixOf` address = Address <$> Success address
-  | otherwise = Failure [FieldError (MustContain "@") which]
+  | otherwise = Failure [FieldError {error = MustContain "@", fieldName = which}]
 
 validateBody :: String -> Validation [FieldError] Body
-validateBody []   = Failure [FieldError MustNotBeEmpty "body"]
-validateBody body = Body <$> Success body
+validateBody [] = Failure [FieldError {error = MustNotBeEmpty, fieldName = "body"}]
+validateBody body' = Body <$> Success body'
 
 validateEmail :: String -> String -> String -> Validation [FieldError] Email
-validateEmail from to body =
-  Email <$> validateAddress from "from-address" <*> validateAddress to "to-address" <*> validateBody body
+validateEmail from' to' body' =
+  Email <$> validateAddress from' "from-address" <*> validateAddress to' "to-address" <*> validateBody body'
